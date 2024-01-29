e536488397e33446841183c712197cb196db701a
cash-i1 <cashignitro@gmail.com>
2024-01-29 15:58:31 +1100

    Initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Red-Blue-pixel-and-Yellow-Pixel-pixel
+A funa pixel game that might make you fly
---
+++ target/debug/deps/Red_Blue_pixel_and_Yellow_Pixel_pixel-27958e77e12f3483.d
@@ -0,0 +1,5 @@
+/media/hdd1/Code/Projects/Red-Blue-pixel-and-Yellow-Pixel-pixel/target/debug/deps/libRed_Blue_pixel_and_Yellow_Pixel_pixel-27958e77e12f3483.rmeta: src/main.rs
+
+/media/hdd1/Code/Projects/Red-Blue-pixel-and-Yellow-Pixel-pixel/target/debug/deps/Red_Blue_pixel_and_Yellow_Pixel_pixel-27958e77e12f3483.d: src/main.rs
+
+src/main.rs:
